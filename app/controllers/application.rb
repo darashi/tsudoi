6d20d1b7794a33bdd9ae44fafcec63b613eac0b0
tmaeda <tmaeda@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2007-10-20 03:08:07 +0000

    acts_as_authenticated を application controller に組み込み。
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@16 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
+  before_filter :login_from_cookie
+
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_tsudoi_session_id'
 end
