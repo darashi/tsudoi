2b82a9e97440cc3e1d9c0a29050ff3ffdf22f946
dara <dara@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2007-10-20 06:44:05 +0000

    定員に関するvalidationを追加 [#2]
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@35 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,12 @@
 class Event < ActiveRecord::Base
   validates_presence_of :name, :date, :location, :description, :status
   belongs_to :owner, :class_name => "User", :foreign_key => :owner_user_id
+  validates_numericality_of :capacity, :only_integer => true, :allow_nil => true
+  def validate
+    if capacity && capacity <= 0
+      errors.add(:capacity, "は1名以上を設定してください")
+    end
+  end
   def before_validation
     self[:status] = "draft"
   end
