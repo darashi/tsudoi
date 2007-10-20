53a0fd86541a97ec0ab333783d0c79f16a0633e3
dara <dara@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2007-10-20 02:21:12 +0000

    sqlite3 で動作しないので options を削除。
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@11 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,9 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
-    options = {
-     :options => "DEFAULT CHARSET=utf8" # 日本語対応にする
-    }
-    create_table :users, options do |t|
+    create_table :users do |t|
       t.column :name, :string
       t.column :mail, :string
       t.column :created_at, :datetime
