3b9b676855ef1ef5cce0f817bc66e8b6eacca8bc
dara <dara@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2007-10-20 10:36:28 +0000

    sqlite3でも動くように変更。
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@60 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- db/migrate/006_add_activation_to_users.rb
+++ db/migrate/006_add_activation_to_users.rb
@@ -3,7 +3,11 @@ class AddActivationToUsers < ActiveRecord::Migration
     add_column :users, :activation_code, :string, :limit => 40
     add_column :users, :activated_at, :datetime
 
-    execute("UPDATE users set activated_at=now() where activated_at is null")
+    User.find(:all).each do |user|
+      if user.activated_at.blank?
+        user.update_attribute :activated_at, Time.now
+      end
+    end
   end
 
   def self.down
