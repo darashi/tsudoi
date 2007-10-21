36a4a06b69ff94d11100dc891d48de6e5ea79535
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2007-10-21 05:47:30 +0000

    開催中のイベント一覧からイベント名によるイベントの検索を行えるようにした
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@99 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
 
+  def event
+    @event_pages, @events = paginate :events, :per_page => 10, :order => 'created_at desc', :conditions => ['name like ?', "%#{params[:event][:name]}%"]
+  end
+
   def people
     @people_pages, @peoples = paginate :users, :per_page => 10, :order => 'created_at desc', :conditions => ['name like ?', "%#{params[:people][:name]}%"]
   end
