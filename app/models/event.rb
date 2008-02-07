8e1f3a763f626913ed8d3c36e70be056a2d6ffac
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-02-07 14:54:33 +0000

    validationを追加
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@149 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :participations
   has_many :members, :through => :participations, :source => :user
+  validates_presence_of :title, :deadline
+  validates_uniqueness_of :title, :case_sensitive => false
 end
