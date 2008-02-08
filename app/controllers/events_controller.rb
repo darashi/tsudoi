ed5746fe68d5163cfa97569c967a44446b945afe
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-02-08 10:59:34 +0000

    #indexの参照を認証不要とした
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@153 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :except => :index  
   # GET /events
   # GET /events.xml
   def index
