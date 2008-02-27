f731a60b8537a5bc24d2c5b7a9c372c72fb6cce2
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-02-27 15:56:13 +0000

    URL、日付関連の検証を追加
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@160 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,10 @@ class Event < ActiveRecord::Base
   has_many :members, :through => :participations, :source => :user
   validates_presence_of :title, :deadline
   validates_uniqueness_of :title, :case_sensitive => false
+  validates_format_of :url, :with => URI.regexp(['http', 'https']), :allow_nil => true
+  validates_each :published_at, :deadline do |model, attr, value|
+    unless value.is_a? DateTime
+      model.errors.add(attr, "には日付を入力してください")
+    end
+  end
 end
