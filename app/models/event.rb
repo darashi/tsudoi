9ff7607ebc406331066eb28224c6d59cc4514a2d
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-03-09 15:39:49 +0000

    スペック実装
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@163 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,33 @@ class Event < ActiveRecord::Base
   validates_uniqueness_of :title, :case_sensitive => false
   validates_format_of :url, :with => URI.regexp(['http', 'https']), :allow_nil => true
   validates_each :published_at, :deadline do |model, attr, value|
-    unless value.is_a? DateTime
-      model.errors.add(attr, "には日付を入力してください")
+    if value
+      unless value.is_a? Time
+        model.errors.add(attr, "には日付を入力してください")
+      end
     end
   end
+
+  def validate_on_update
+    if date?(deadline)
+      if date?(published_at) && (deadline <= published_at)
+        errors.add(:published_at, "が正しくありません")
+      end
+    end
+  end
+
+  def validate
+    errors.add(:deadline, "が正しくありません") if date?(deadline) && (deadline <= Time.now)
+  end
+
+  def before_save
+    self.published_at = Time.now unless self.published_at
+  end
+
+  private
+
+  def date?(obj)
+    obj && obj.respond_to?("to_date")
+  end
+
 end
