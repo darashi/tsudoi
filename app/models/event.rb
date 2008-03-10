210fa9d313fc30dc3ec2a80baf691dd8efb5c808
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-03-10 09:10:39 +0000

    従属に関するスペックに対応
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@168 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+  belongs_to :owner, :class_name => "User", :foreign_key => :owner_user_id
   has_many :participations
   has_many :members, :through => :participations, :source => :user
   validates_presence_of :title, :deadline
