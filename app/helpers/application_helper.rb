b5152cddfcd9987aaef228f1046931eb95f50525
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-03-13 07:55:56 +0000

    画面周りを復元
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@182 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_to_event(event)
+    link_to(h(event.title), :controller => :events, :action => :show, :id => event.id)
+  end
 end
