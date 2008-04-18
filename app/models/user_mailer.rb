ea18fbfe306f8f3d771309abcd54d470ee8ba005
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-04-18 08:55:55 +0000

    メールの文面を日本語に修正
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@224 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,13 +4,13 @@ class UserMailer < ActionMailer::Base
 
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
+    @subject    += 'ユーザ登録確認'
     @body[:url]  = url_for(:controller => "users", :action => :activate, :activation_code => user.activation_code)
   end
 
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
+    @subject    += 'ユーザ登録完了'
     @body[:url]  = url_for(:controller => "/")
   end
 
