2b4ba66b4c6d56431d859e352e60696c32a0be2a
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-05-02 10:21:30 +0000

    イベント情報にポジションペーパ管理URLを追加
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@228 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

---
+++ db/migrate/007_add_position_paper_url_to_events.rb
@@ -0,0 +1,9 @@
+class AddPositionPaperPageToEvents < ActiveRecord::Migration
+  def self.up
+    add_column :events, :position_paper_url, :string
+  end
+
+  def self.down
+    remove_column :events, :position_paper_url
+  end
+end
