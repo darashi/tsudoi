ffe345286abcda76696ab2eeb73f69d60e9aca2d
snoozer05 <snoozer05@2965336b-9d0c-4aaa-adc1-bee1224dcb4e>
2008-05-02 14:59:36 +0000

    アカウント情報の変更を追加
    git-svn-id: https://svn.ruby-sapporo.org/tsudoi/trunk@232 2965336b-9d0c-4aaa-adc1-bee1224dcb4e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,12 @@ class UsersController < ApplicationController
   # render new.rhtml
   def new
   end
-
+  
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def create
     cookies.delete :auth_token
     # protects against session fixation attacks, wreaks havoc with 
@@ -27,6 +32,23 @@ class UsersController < ApplicationController
     flash[:notice] = "Thanks for signing up! Please check your email to activate your account before logging in."
   rescue ActiveRecord::RecordInvalid
     render :action => 'new'
+  end
+  
+  # PUT /users/1
+  # PUT /users/1.xml
+  def update
+    @user = User.find(params[:id])
+    
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_back_or_default('/home') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def activate
